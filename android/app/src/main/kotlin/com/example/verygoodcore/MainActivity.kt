5f71d08c769c510c84abd9a620ad1c5bfc1b15e4
Rodrigo Castro <rodrigo.castro@avira.com>
2022-03-02 17:47:18 +0100

    First commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.linkedin_post_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2021, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:linkedin_post_generator/app/app.dart';
+import 'package:linkedin_post_generator/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
